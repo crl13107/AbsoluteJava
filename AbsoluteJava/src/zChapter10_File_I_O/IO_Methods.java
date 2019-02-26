0ce53a100779406d131204cf9c61b82e677b988b
Colten <Colten@151.161.250.203>
2019-02-26 10:36:43 -0500

    Chapter 10 Starter Classes

---
+++ AbsoluteJava/src/zChapter10_File_I_O/IO_Methods.java
@@ -0,0 +1,8 @@
+package zChapter10_File_I_O;
+
+public class IO_Methods {
+
+	public static void intrdocution() {
+		
+	}
+}
---
+++ AbsoluteJava/src/zChapter10_File_I_O/Inrtoduction.java
@@ -0,0 +1,8 @@
+package zChapter10_File_I_O;
+
+public class Inrtoduction {
+
+	public static void main(String[] args) {
+		IO_Methods.intrdocution();
+	}
+}
